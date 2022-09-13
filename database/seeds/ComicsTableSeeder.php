b2456db3ab023dd3bfcb129e39d41d1fadac6f8f
amaljom <amaljom99@gmail.com>
2022-09-13 15:45:34 +0200

    utilizzato il faker per popolare il db

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -1,16 +1,30 @@
 <?php
 
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
+use App\comic;
 
 class ComicsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
+     *$table->id();
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        
+        $type= 'comic-book';
+        for ($i=0; $i < 10; $i++) { 
+            $newComic = new Comic();
+            $newComic->title = $faker->realText(30);
+            $newComic->description = $faker->paragraph(2, true);
+            $newComic->thumb = $faker->imageUrl(400, 400, 'comics');
+            $newComic->price= $faker->randomDigit();
+            $newComic->series= $faker->name();
+            $newComic->sale_date = $faker->date();
+            $newComic->type =$type;
+
+            $newComic->save();
+        }
     }
 }
