c3258b24dfd32bd7a49860b7ddd5f321f9358130
amaljom <amaljom99@gmail.com>
2022-09-14 16:10:54 +0200

    primo tentativo di creare lo store

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -35,7 +35,7 @@ class ComicsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
--- resources/views/main/Create.blade.php
+++ resources/views/main/Create.blade.php
@@ -1,28 +1,30 @@
 @extends('home')
 
 @section('main-content')
-<form class="form1" action="" method="POST">
+<form class="form1" action=" {{ route('store') }} " method="POST">
     @csrf
     <label for="title">Title</label>
     <input type="text" name="title" id="title">
     
-    <label for="title">description</label>
-    <input type="text-area" name="description" id="description">
+    <label for="description">description</label>
+    <textarea type="text" name="description" id="description">  </textarea>
    
-    <label for="title">thumb</label>
+    <label for="thumb">thumb</label>
     <input type="url" name="thumb" id="thumb">
 
-    <label for="title">price</label>
+    <label for="price">price</label>
     <input type="text" name="price" id="price">
 
-    <label for="title">series</label>
+    <label for="series">series</label>
     <input type="text" name="series" id="series">
 
-    <label for="title">sale_date</label>
+    <label for="sale_date">sale_date</label>
     <input type="date" name="sale_date" id="sale_date">
 
-    <label for="title">type</label>
+    <label for="type">type</label>
     <input type="text" name="type" id="type">
+
+    <input type="submit" value="invia">
 </form>
     
 @endsection
