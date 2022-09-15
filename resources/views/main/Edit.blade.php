8790fa32f78fade9dc01d460b41c35ee6ddd5e1a
amaljom <amaljom99@gmail.com>
2022-09-15 18:04:22 +0200

    aggiunti i value nel blade edit

--- resources/views/main/Edit.blade.php
+++ resources/views/main/Edit.blade.php
@@ -6,25 +6,25 @@
 
     @method('PUT')
     <label for="title">Title</label>
-    <input type="text" name="title" id="title">
+    <input type="text" name="title" id="title" value="{{ $Comic->title }}">
     
     <label for="description">description</label>
-    <textarea type="text" name="description" id="description">  </textarea>
+    <textarea type="text" name="description" id="description"> {{ $Comic->description }}"  </textarea>
    
     <label for="thumb">thumb</label>
-    <input type="url" name="thumb" id="thumb">
+    <input type="url" name="thumb" id="thumb" value="{{ $Comic->thumb }}">
 
     <label for="price">price</label>
-    <input type="text" name="price" id="price">
+    <input type="text" name="price" id="price" value="{{ $Comic->price }}">
 
     <label for="series">series</label>
-    <input type="text" name="series" id="series">
+    <input type="text" name="series" id="series" value="{{ $Comic->series }}">
 
     <label for="sale_date">sale_date</label>
-    <input type="date" name="sale_date" id="sale_date">
+    <input type="date" name="sale_date" id="sale_date" value="{{ $Comic->sale_date }}">
 
     <label for="type">type</label>
-    <input type="text" name="type" id="type">
+    <input type="text" name="type" id="type" value="{{ $Comic->type }}">
 
     <input type="submit" value="invia">
 </form>
